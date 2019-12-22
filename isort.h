f562387ce0ea1148ffeac82956e9d78762fbfded
sharonbenaharon <58299693+sharonbenaharon@users.noreply.github.com>
2019-12-22 16:53:25 +0200

    Add files via upload

---
+++ isort.h
@@ -0,0 +1,16 @@
+#ifndef _ISORT_H
+#define _ISORT_H
+#define LEN 50
+#include <stdio.h>
+
+FILE* f;
+int arr[LEN] ;
+
+shift_element(int* arr, int i);
+insertion_sort(int* arr, int len);
+
+
+
+
+
+#endif
