723d05ba7f4c9f54e3ab7023e5a1545f0c40f97c
sharonbenaharon <58299693+sharonbenaharon@users.noreply.github.com>
2019-12-22 21:07:06 +0200

    Add files via upload

---
+++ mainSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "isort.h"
+
+
+int main() {
+	char fileName[100];
+	printf("Enter the file name:\n");
+	scanf("%s", fileName);
+	f = fopen(fileName, "r");
+	if (f == NULL) {
+		printf("Error cannot open %s", fileName);
+		exit(1);
+	}
+	if (f != NULL) {
+		for (int j = 0; j < LEN; j++) {
+			fscanf(f, "%1d", (arr + j));
+		}
+	}
+	fclose(f);
+	int arrSave[LEN];
+	for (int k = 0; k < LEN; k++) {
+		*(arrSave + k) = *(arr + k);
+	}
+	int number;
+	printf("enter a number to first:\n");
+	scanf("%d", &number);
+	shift_element(arr, number);
+	for (int k = 0; k < LEN; k++) {
+		*(arr + k) = *(arrSave + k);
+	}
+	insertion_sort(arr, LEN);
+
+	return -1;
+}
