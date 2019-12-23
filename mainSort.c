04e39cdf9584e2e82c2eacff706dcea0d8beb3c9
sharonbenaharon <58299693+sharonbenaharon@users.noreply.github.com>
2019-12-23 12:53:24 +0200

    Update mainSort.c

--- mainSort.c
+++ mainSort.c
@@ -3,24 +3,11 @@
 
 
 int main() {
-	char fileName[100];
-
+	
 		for (int j = 0; j < LEN; j++) {
 			scanf("%d", (arr + j));
 		}
-	
-	int arrSave[LEN];
-	for (int k = 0; k < LEN; k++) {
-		*(arrSave + k) = *(arr + k);
-	}
-	int number;
-	printf("enter a number to first:\n");
-	scanf("%d", &number);
-	shift_element(arr, number);
-	for (int k = 0; k < LEN; k++) {
-		*(arr + k) = *(arrSave + k);
-	}
 	insertion_sort(arr, LEN);
-
+	print(arr);
 	return 0;
 }
