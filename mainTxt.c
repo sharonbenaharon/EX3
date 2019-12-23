10e78769afead7fea20769a7cb52985384f55da3
sharonbenaharon <58299693+sharonbenaharon@users.noreply.github.com>
2019-12-23 18:37:31 +0200

    Update mainTxt.c

--- mainTxt.c
+++ mainTxt.c
@@ -1,56 +1,59 @@
 #include <stdio.h>
-#include <string.h>                                                            
-#include <stdlib.h>                                                            
-#include <ctype.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "txtfind.h"
 
-
-
-int main() {
+int main()
+{
 
 	char inputOP;
 	char inputWord[WORD_LEN_MAX];
 	char w[WORD_LEN_MAX];
 	char s[LINE_LEN_MAX];
 	int ansLine;
-	
+
 	int lenA = 1;
 	int ans;
 	int i = 0;
-        int n;
+	int n;
 
-	
-		getWord(w);
-		strcpy(inputWord, w);
-		getWord(w);
-		inputOP = *(w + i);
-	
-		if (inputOP == 'b') {
+	getWord(w);
+	strcpy(inputWord, w);
+	getWord(w);
+	inputOP = *(w + i);
+
+	if (inputOP == 'b')
+	{
+		ans = getWord(w);
+		n = 1;
+		while (lenA != 0)
+		{
 			ans = getWord(w);
-			n=1;
-			while (lenA!=0) {
-				ans = getWord(w);
-				int ansWord = similarCheck(w, inputWord, n);
-				if (ansWord == 1) {
+			int ansWord = similarCheck(w, inputWord, n);
+			if (ansWord == 1)
+			{
 				printWordSimilar(w);
-				}
-				lenA = ans;
 			}
-			
+			lenA = ans;
 		}
-		int lenB = 1;
-		if (inputOP == 'a') {
-			while (lenB != 0) {
-				ans = getLine(s);
-				ansLine = containStringCheck(s, inputWord);
-				if (ansLine == 1) {
-					printLineSimilar(s);
-				}
-				lenB = ans;
+	}
+	int lenB = 1;
+	if (inputOP == 'a')
+	{
+		getLine(s);
+		while (lenB != 0)
+		{
+			ans = getLine(s);
+			ansLine = containStringCheck(s, inputWord);
+			if (ansLine == 1)
+			{
+				printLineSimilar(s);
 			}
-			
+			lenB = ans;
 		}
-	
+	}
 
 	return 0;
 }
+
